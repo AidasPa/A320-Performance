165f5f3677e7fa61b7bbf7d8ec903d7c5cd23f00
Aidas <aidas@bitlgx.com>
2020-06-30 18:37:12 +0300

    improved and broken airport search

--- services/search_ident.php
+++ services/search_ident.php
@@ -20,19 +20,34 @@ $airportsRaw = file_get_contents('../data/airports.txt');
 $data = json_decode(file_get_contents('php://input'));
 
 $airports = explode("\n", $airportsRaw);
-$airportOnlyIATAs = [];
+$airportOnlyICAOs = [];
 
 foreach ($airports as $airport) {
-    $airportOnlyIATAs[] = explode('/', $airport)[0];
+    $airportOnlyICAOs[] = explode('/', $airport)[0];
 }
 
-$input = preg_quote($data->query, '~');
-$result = preg_filter('~' . $input . '~', null, $airportOnlyIATAs);
+// $input = preg_quote($data->query, '~');
+// $result = preg_filter('~' . $input . '~', null, $airportOnlyICAOs);
+
+// $searchResult = [];
+// foreach ($result as $key => $value) {
+//     $searchResult[] = $airports[$key];
+// }
+
+// echo json_encode($searchResult);
 
 $searchResult = [];
-foreach ($result as $key => $value) {
-    $searchResult[] = $airports[$key];
+
+foreach ($airportOnlyICAOs as $key => $airport) {
+    if (strlen($data->query) === 2) {
+        $firstTwo = substr($airport, 0, 2);
+        if($firstTwo === $data->query) $searchResult[] = $airports[$key];
+    } else if (strlen($data->query) === 3) {
+        $firstThree = substr($data->query, 0, 3);
+        if($firstThree === $data->query) $searchResult[] = $airports[$key];
+    } else if (strlen($data->query) === 4) {
+        if($airport === $data->query) $searchResult[] = $airports[$key];
+    }
 }
 
 echo json_encode($searchResult);
-
